ada30d16aa5b4f8ce99898e7ad72d42fbf0fc57d
Tardis2001 <matheus_santos_evangelista@hotmail.com>
2022-05-09 18:54:51 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/Tardis2001.github.io/'
+  ? 'https://matheusevangelista.netlify.app/'
   : '/'
 })
