179529b6a0d070b3fb5d63577ce35761a5badf70
Tardis2001 <matheus_santos_evangelista@hotmail.com>
2022-06-06 21:07:51 -0300

    Update vue.config.js
    Esqueci de atualizar a rota

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
   ? 'https://master.d1obi0hfjntner.amplifyapp.com/'
-  : '/'
+  : '/matheusevangelista.xyz'
 })
